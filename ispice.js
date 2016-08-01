10b7333a05af220e893adc617834a64cd82025df
Isaac Schofield <isaac.merrus@hotmail.co.uk>
2016-08-01 18:38:39 +0100

    Complete potential aesthetic changes
    The potential change in aeasthetic seen in the previous commit has been applied to all segments of the page, with the previously implemented alternating L/R remaining. A Twitter feed has also been added, and the structure of the site under the hood has changed, with a 'scripts' folder being added to contain jQuery, the Twitter module, the ScrollTo module (to hopefully be used in future to aid the function of the navbar and tooltip), and of course the external JavaScript.

--- ispice.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    setTimeout(fade, 2000);
-}
-function fade(){
-   $("#scrollrem").fadeOut(1000);
-}
---
+++ scripts/ispice.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#scrollrem').delay(4000).fadeOut(3000);
+  $('#scrollrem-overlay').delay(4000).fadeOut(3000);
+});
+$('#navhome').click(function(){
+  $(window).scrollTo('500px');
+});
